91961d9f678e736dbc33119b512d9ffbb33cb2f1
Khanzaadeh <khanzadeh008@gmail.com>
2021-08-23 23:08:33 +0600

    Changed flutter sdk version to 2.10 and added a new file called expressions in bin.

---
+++ dart/bin/expressions/statements_exp_op.dart
@@ -0,0 +1,20 @@
+void main() {
+  // it is a statement :)
+  print('Hello World');
+
+//expressions are boring.
+
+//arithmatic operations.
+
+  print(1 + 2);
+  print(1 - 1);
+  print(1 * 4);
+  print(1 / 2);
+
+  // useful tip: for integer div just add ~ before the operator.
+
+  print(4 / 3); // will return a double not an int div
+  print(4 ~/ 3); // will return an int div like java.
+
+  
+}
