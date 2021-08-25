ddbd903228212256c1f1b26f50a40f52d3b9076f
Khanzaadeh <khanzadeh008@gmail.com>
2021-08-25 17:06:10 +0600

    Added data types.

---
+++ dart/bin/datatypes/data_types.dart
@@ -0,0 +1,39 @@
+/**
+ * the main/root data type in dart is called object. 
+ */
+
+//type conversion
+
+var age = 30;
+
+var ageString = age.toString();
+
+var ageDouble = age.toDouble();
+
+var price = '100';
+
+var priceDouble = double.parse(price);
+
+var priceInt = int.parse(price);
+
+// double will always store int but int wont store double
+// if you multiply a double with an int the value will have type double.
+
+int value1 = 3;
+
+double value2 = 2;
+
+var result = value1 * value2;
+
+void main(List<String> args) {
+  print('The result is: ${result.toString()}');
+
+  print(valueInt.isEven);
+}
+
+//type casting:
+
+num value = 7;
+
+var valueInt = value as int;
+
